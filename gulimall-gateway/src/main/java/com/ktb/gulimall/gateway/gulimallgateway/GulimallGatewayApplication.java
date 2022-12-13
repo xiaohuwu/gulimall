9ae4569094a3e50c2ab589eccad6765b6d65be6d
xiaohu <1205150638@qq.com>
2022-12-13 16:08:31 +0800

    初始化样式

--- gulimall-gateway/src/main/java/com/ktb/gulimall/gateway/gulimallgateway/GulimallGatewayApplication.java
+++ gulimall-gateway/src/main/java/com/ktb/gulimall/gateway/gulimallgateway/GulimallGatewayApplication.java
@@ -1,11 +1,15 @@
 package com.ktb.gulimall.gateway.gulimallgateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 public class GulimallGatewayApplication {
 
     public static void main(String[] args) {
--- gulimall-gateway/src/test/java/com/ktb/gulimall/gateway/gulimallgateway/GulimallGatewayApplicationTests.java
+++ gulimall-gateway/src/test/java/com/ktb/gulimall/gateway/gulimallgateway/GulimallGatewayApplicationTests.java
@@ -1,6 +1,6 @@
 package com.ktb.gulimall.gateway.gulimallgateway;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
